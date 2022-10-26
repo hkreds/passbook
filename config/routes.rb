cd224bc0d028ee7f8b4d247d7665ed2d9f38fbb1
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-26 03:08:55 +0000

    [Add]ログイン時の挙動、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'bank_passbooks/index'
-  get 'bank_passbooks/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
+  devise_for :users
+  resources :users, only:[:index,:show]
+  resources :bank_passbooks, only:[:new,:create,:show,:destroy]
+  resources :passbook_historys, only:[:create]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
