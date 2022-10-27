5ef708cc3bd1eea0b02408eef6402262bfcb192c
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-27 08:02:30 +0000

    [Add]口座番号のバリデーション

--- app/models/bank_passbook.rb
+++ app/models/bank_passbook.rb
@@ -1,6 +1,6 @@
 class BankPassbook < ApplicationRecord
   belongs_to :user
   has_many :passbook_histories, dependent: :destroy
-  validates :account_number, length: { is: 7 }
+  validates :account_number, numericality: {only_integer: true}, length: { is: 7 }, uniqueness: {scope: [:user_id]}
   validates :balance, presence: true
 end
