3887be96b0bb79ede1f40205ae1f372fb6a443a8
hkreds <109258421+hkreds@users.noreply.github.com>
2022-10-28 08:16:27 +0000

    [Add]桁区切り、ユーザー詳細にて口座数と合計残高表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @bank_passbooks = @user.bank_passbooks
     @bank_passbook = BankPassbook.new
+    @user_all_balance = 0
+    @bank_passbooks.each do |bank_passbook|
+      @user_all_balance += bank_passbook.balance
+    end
   end
 end
